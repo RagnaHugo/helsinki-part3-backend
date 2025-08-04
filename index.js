40e2a5138d1d98f22cebf348500f8bab864f9e2c
RagnaHugo <123764597+RagnaHugo@users.noreply.github.com>
2025-08-04 16:59:37 -0500

    exercise 3.7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 let persons = [
   {
@@ -25,6 +26,7 @@ let persons = [
 
 const app = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/", (request, response) => {
