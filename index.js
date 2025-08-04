afd72d8a8b7369dd331314d80e4b27721e68c6c0
RagnaHugo <123764597+RagnaHugo@users.noreply.github.com>
2025-08-04 13:57:07 -0500

    exercise 3.2

--- index.js
+++ index.js
@@ -33,6 +33,14 @@ app.get("/api/persons/", (request, response) => {
   response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+  response.send(
+    `<p>PhoneBook has info for ${
+      persons.length
+    } people</p><p>${new Date().toString()}</p>`
+  );
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
