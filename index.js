8bc5ba676e54849882c6f43b9930c5e044e8a43f
RagnaHugo <123764597+RagnaHugo@users.noreply.github.com>
2025-08-05 14:19:23 -0500

    add dist folder

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ app.post("/api/persons/", (request, response) => {
   response.status(201).json(contact);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("Servidor iniciado");
+  console.log(`Server running on port ${PORT}`);
 });
