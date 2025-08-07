d7753b79c6b18cfef89008500271cdb93181662f
RagnaHugo <123764597+RagnaHugo@users.noreply.github.com>
2025-08-07 10:03:13 -0500

    exercise 3.12

---
+++ mongo.js
@@ -0,0 +1,44 @@
+const { mongoose, model } = require("mongoose");
+const { Schema } = mongoose;
+
+const password = process.argv[2];
+const url = `mongodb+srv://hugozjimi:${password}@cluster0.j16qww0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Base conectada");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const newSchema = new Schema({
+  name: String,
+  phone: String,
+});
+const ModelContact = model("Contact", newSchema);
+
+if (process.argv.length > 3) {
+  const contacto = new ModelContact({
+    name: process.argv[3],
+    phone: process.argv[4],
+  });
+
+  contacto
+    .save()
+    .then((res) => {
+      console.log(res);
+      mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+} else {
+  ModelContact.find({}).then((res) => {
+    res.forEach((element) => {
+      console.log(element);
+    });
+    mongoose.connection.close();
+  });
+}
